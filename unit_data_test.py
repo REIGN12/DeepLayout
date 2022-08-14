b771afd9442ff7c8f923cc512e514df9fa8e4cac
reign <3180105323@zju.edu.cn>
2022-08-14 20:47:13 +0800

    adding unit_data_test for data processing

---
+++ unit_data_test.py
@@ -0,0 +1,43 @@
+import typer
+from pathlib import Path
+import os
+import json
+
+def isleaf_dir(abs_path:Path):
+    return any(f.endswith(".json") for f in os.listdir(abs_path))
+
+def traverse(cur_abs_path:Path):
+    if isleaf_dir(cur_abs_path):
+        print(f"{cur_abs_path}:")
+        json_files = [f for f in os.listdir(cur_abs_path) if f.endswith(".json")]
+        for json_file in json_files:
+            json_path = cur_abs_path/json_file
+            print(f"{json_path}:")
+            with open(json_path,"r") as fin:
+                data = json.load(fin)[0]
+                slides = data['slides']
+                for slide in slides:
+                    shapes = slide['shapes']
+                    seq = []
+                    for shape in shapes:
+                        cate = shape['Type']
+                        pos0 = shape['pos0']
+                        pos1 = shape['pos1']
+                        print(cate,*pos0,*pos1)
+                    print()
+    else:
+        for childdir in os.listdir(cur_abs_path):
+            childdir_path = cur_abs_path/childdir
+            if os.path.isdir(childdir_path):
+                traverse(cur_abs_path/childdir)
+
+app = typer.Typer()
+@app.command()
+def run(dir_path:Path):
+    traverse(dir_path)
+    
+
+if __name__ == "__main__":
+    app()
+
+
