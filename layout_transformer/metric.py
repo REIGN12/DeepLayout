7d8d8020017bd4d15edabad9ab01c98ba5a2f5ef
reign <3180105323@zju.edu.cn>
2022-09-23 15:36:44 +0800

    Support seqs2bbox for metric

--- layout_transformer/metric.py
+++ layout_transformer/metric.py
@@ -3,6 +3,8 @@ import torch
 from torch import Tensor
 from einops import rearrange
 
+from typing import Tuple
+
 # cp from https://github.com/rwightman/pytorch-image-models/timm/utils/metrics.py
 class AverageMeter:
     """Computes and stores the average and current value"""
@@ -20,6 +22,42 @@ class AverageMeter:
         self.sum += val * n
         self.count += n
         self.avg = self.sum / self.count
+
+def seqs2bboxes(seqs:Tensor,eos:int)->Tuple[Tensor,Tensor]:
+    """
+    seqs -> bboxs, bbox_nums
+    seqs:(B,TL)
+    bboxs: (B,N,4)
+    bbox_nums:(B,)
+    >>> seqs = torch.tensor([
+    ... [100,1,1,1,1,1,101,101,0,0,0,0],
+    ... [100,1,1,1,1,1,2,2,2,2,2,101],
+    ... ])
+    >>> eos = 101
+    >>> bboxs,bbox_nums = seqs2bboxes(seqs,eos)
+    >>> bboxs
+    tensor([[[  1,   1,   1,   1],
+             [101,   0,   0,   0]],
+    <BLANKLINE>
+            [[  1,   1,   1,   1],
+             [  2,   2,   2,   2]]])
+    >>> bbox_nums
+    tensor([[1],
+            [2]])
+    """
+    B,TL = seqs.shape
+    seqs = seqs.clone()[:,1:] # rm <bos>
+    seq_lengths = ( (seqs==eos).int() * reversed(torch.arange(1,TL))[None,:] ).argmax(-1,keepdim=True)
+
+    if torch.any(seq_lengths % 5 != 0):
+        import warnings
+        warnings.warn("some seq_len % 5 is not 0!",RuntimeWarning)
+    bbox_nums = seq_lengths // 5 
+
+    N = torch.max(bbox_nums).item()
+    bboxes = rearrange(seqs[:,:N * 5],'b (n c) -> b n c',c=5)[...,1:]
+    return bboxes,bbox_nums
+
 
 def compute_overlap(bboxes:Tensor,bbox_nums:Tensor)->float:
     """
